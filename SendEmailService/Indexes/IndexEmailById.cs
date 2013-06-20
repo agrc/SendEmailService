d7519ec9d2eb67301679a462bec4cad6f283b022
steve <sgourley@utah.gov>
2013-06-20 15:20:59 -0600

    New index

---
+++ SendEmailService/Indexes/IndexEmailById.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Raven.Client.Indexes;
+using SendEmailService.Models;
+
+namespace SendEmailService.Indexes
+{
+    public class IndexEmailById: AbstractIndexCreationTask<Emails>
+    {
+        public IndexEmailById()
+        {
+            Map = xx => from x in xx
+                        select new
+                        {
+                            x.Id
+                        };
+        }
+    }
+}
