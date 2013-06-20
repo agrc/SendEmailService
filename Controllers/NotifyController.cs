c66697ee71926acd02d6f9d646744c6a0625496d
steve <sgourley@utah.gov>
2013-06-20 11:40:45 -0600

    iis config

--- Controllers/NotifyController.cs
+++ Controllers/NotifyController.cs
@@ -1,35 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Web.Http;
+﻿using System.Web.Http;
+using AttributeRouting.Web.Http;
 
 namespace SendEmailService.Controllers
 {
     public class NotifyController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
+        [POST("{id")]
         public string Get(int id)
         {
             return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
         }
     }
 }
