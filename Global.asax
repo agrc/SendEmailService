6222206c454bdf8f019c0c6e39021afc2a048476
steve <sgourley@utah.gov>
2013-06-20 11:34:47 -0600

    empty project

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SendEmailService.WebApiApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SendEmailService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+    }
+}
