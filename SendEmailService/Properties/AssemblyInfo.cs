75a56bf88d1e55ddb9fee02f2a79a1c447eb7e3d
steveoh <sgourley@utah.gov>
2014-08-12 12:59:41 -0600

    Release 0.1.0812.3

--- SendEmailService/Properties/AssemblyInfo.cs
+++ SendEmailService/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("SendEmailService")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyCopyright("Copyright ©AGRC 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.1.0625.2")]
-[assembly: AssemblyFileVersion("0.1.0625.2")]
+[assembly: AssemblyVersion("0.1.0812.3")]
+[assembly: AssemblyFileVersion("0.1.0812.3")]
